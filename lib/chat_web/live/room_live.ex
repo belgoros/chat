869ad8de2549230887933a824d199f6ab1e7a3d5
Serguei Cambour <s.cambour@gmail.com>
2021-06-08 14:03:56 +0200

    Identify whi sends a message to the chat

--- lib/chat_web/live/room_live.ex
+++ lib/chat_web/live/room_live.ex
@@ -5,21 +5,24 @@ defmodule ChatWeb.RoomLive do
   @impl true
   def mount(%{"id" => room_id}, _session, socket) do
     topic = "room" <> room_id
+    username = MnemonicSlugs.generate_slug(2)
+
     if connected?(socket), do: ChatWeb.Endpoint.subscribe(topic)
 
     {:ok,
       assign(socket,
         room_id: room_id,
         topic: topic,
+        username: username,
         message: "",
-        messages: [%{uuid: UUID.uuid4(), content: "Twitch joined the chat"}],
+        messages: [%{uuid: UUID.uuid4(), content: "#{username} joined the chat", username: "system"}],
         temporary_assigns: [messages: []]
       )}
   end
 
   @impl true
   def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
-    message = %{uuid: UUID.uuid4(), content: message}
+    message = %{uuid: UUID.uuid4(), content: message, username: socket.assigns.username}
     ChatWeb.Endpoint.broadcast(socket.assigns.topic, "new-message", message)
     {:noreply, assign(socket, message: "")}
   end
