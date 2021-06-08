cc66dc9e0a6a4312ef74a7f33a3c9242bb3b9188
Serguei Cambour <s.cambour@gmail.com>
2021-06-08 12:46:36 +0200

    Display new chat messages

--- lib/chat_web/live/room_live.ex
+++ lib/chat_web/live/room_live.ex
@@ -4,6 +4,27 @@ defmodule ChatWeb.RoomLive do
 
   @impl true
   def mount(%{"id" => room_id}, _session, socket) do
-    {:ok, assign(socket, room_id: room_id)}
+    topic = "room" <> room_id
+    if connected?(socket), do: ChatWeb.Endpoint.subscribe(topic)
+
+    {:ok,
+      assign(socket,
+        room_id: room_id,
+        topic: topic,
+        messages: [%{uuid: UUID.uuid4(), content: "Twitch joined the chat"}],
+        temporary_assigns: [messages: []]
+      )}
+  end
+
+  @impl true
+  def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
+    message = %{uuid: UUID.uuid4(), content: message}
+    ChatWeb.Endpoint.broadcast(socket.assigns.topic, "new-message", message)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(%{event: "new-message", payload: message}, socket) do
+    {:noreply, assign(socket, messages: [message])}
   end
 end
